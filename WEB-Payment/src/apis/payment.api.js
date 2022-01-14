17d601d6077e38e37df515bb59b70ce8b988b7dd
iamduy17 <thaiduydo17@gmail.com>
2022-01-14 10:14:13 +0700

    merge payment + update UI of product packets

--- WEB-Payment/src/apis/payment.api.js
+++ WEB-Payment/src/apis/payment.api.js
@@ -1,1 +1,7 @@
-//const router = require("express").Router();
+const express = require("express"),
+  router = express.Router(),
+  paymentC = require("../controllers/payment.C");;
+
+router.put("/", paymentC.putPayment);
+router.post("/", paymentC.postPayment);
+module.exports = router;
