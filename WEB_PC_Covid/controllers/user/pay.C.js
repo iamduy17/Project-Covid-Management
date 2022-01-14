1adc0fe7fb4147a028b7c3a5bf5528bcc919da28
DNam018 <95735490+DNam018@users.noreply.github.com>
2022-01-14 11:16:06 +0700

    change ui pc_vovid_pay + port

--- WEB_PC_Covid/controllers/user/pay.C.js
+++ WEB_PC_Covid/controllers/user/pay.C.js
@@ -3,16 +3,39 @@ const express = require('express'),
 
 router.get('/', (req, res) => {
   res.render('user/pay/pay', {
-    title: 'Thanh toán chi phí',
+    title: 'Internet Banking',
     active: { pay: true },
   });
 });
 
 router.get('/payDetail', (req, res) => {
   res.render('user/pay/payDetail', {
-    title: 'Thanh toán chi phí',
+    title: 'Internet Banking',
+    active: { pay: true },
+    layout: false,
+  });
+});
+
+router.get('/changePass', (req, res) => {
+  res.render('user/pay/changePass', {
+    title: 'Internet Banking',
     active: { pay: true },
   });
 });
+
+router.get('/payment', (req, res) => {
+  res.render('user/pay/payment', {
+    title: 'Internet Banking',
+    active: { pay: true },
+  });
+});
+
+router.get('/recharge', (req, res) => {
+  res.render('user/pay/recharge', {
+    title: 'Internet Banking',
+    active: { pay: true },
+  });
+});
+
 
 module.exports = router;
